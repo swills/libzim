dd5a501c7d22ec348d04cf5239f7801f844e3410
Veloman Yunkan <veloman.yunkan@gmail.com>
2020-06-30 23:37:25 +0400

    test/tempfile.h
    Moved TempFile from test/cluster.cpp to a header file of its own so that
    it can be utilized in other unit-tests too.

--- test/cluster.cpp
+++ test/cluster.cpp
@@ -55,49 +55,12 @@
 #include "../src/endian_tools.h"
 #include "../src/config.h"
 
+#include "tempfile.h"
+
 namespace
 {
 
-class TempFile
-{
-  int fd_;
-#ifndef _WIN32
-  std::string path_;
-#endif
-public:
-  explicit TempFile(const char* name)
-  {
-#ifdef _WIN32
-    const int n = strlen(name);
-    wchar_t cbase[MAX_PATH];
-    wchar_t ctmp[MAX_PATH];
-    wchar_t wname[MAX_PATH];
-    mbstowcs(wname, name, n+1);
-    GetTempPathW(MAX_PATH-(n+2), cbase);
-    // This create a file for us, ensure it is unique.
-    // So we need to delete it and create the directory using the same name.
-    GetTempFileNameW(cbase, wname, 0, ctmp);
-    auto tmp_fd = _wopen(ctmp, _O_CREAT | _O_TEMPORARY | _O_SHORT_LIVED | _O_RDWR | _O_TRUNC);
-#else
-    const char* const TMPDIR = std::getenv("TMPDIR");
-    const std::string tmpdir(TMPDIR ? TMPDIR : "/tmp");
-    path_ = tmpdir + "/" + name + "_XXXXXX";
-    auto tmp_fd = mkstemp(&path_[0]);
-#endif
-    fd_ = tmp_fd;
-  }
-
-  TempFile(const TempFile& ) = delete;
-
-  ~TempFile()
-  {
-#ifndef _WIN32
-    unlink(path_.c_str());
-#endif
-  }
-
-  int fd() const { return fd_; }
-};
+using zim::unittests::TempFile;
 
 std::shared_ptr<zim::Buffer> write_to_buffer(zim::writer::Cluster& cluster)
 {
